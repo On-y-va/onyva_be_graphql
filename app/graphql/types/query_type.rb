cd5f99cc40630c18424fa80ac6291e7d014afc0b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-21 11:21:18 -0600

    Create one user endpoint

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,16 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :all_users, [Types::UserType], null: false
+    field :one_user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
 
-      
     def all_users
       User.all
+    end
+    
+    def one_user(id:)
+      User.find(id)
     end
   end
 end
